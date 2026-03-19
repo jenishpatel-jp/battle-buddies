0c78c38e48781eddc14e869ca13a9415e57e4aa8
Jenish <jpat534@gmail.com>
2026-03-19 18:16:17 +1100

    Added Border History component and added it to the main page

---
+++ components/battle-history/BattleHistory.tsx
@@ -0,0 +1,9 @@
+const BattleHistory = () => {
+  return (
+    <div className="flex flex-col border-2 border-blue-500 rounded-lg p-4 m-4 w-full max-w-md">
+        <h2 className="text-black">Battle History</h2>
+    </div>
+  )
+}
+
+export default BattleHistory
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import BattleHistory from "../../components/battle-history/BattleHistory";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1 className="text-4xl text-white">Battle Buddies!</h1>
+    <div className="flex flex-col h-1/6 items-center justify-center bg-zinc-50 font-sans ">
+      
+      <main className="flex min-h-screen flex-col items-center justify-center py-32 px-16 bg-zinc sm:items-start w-full">
+        <h1 className="text-4xl w-full text-black justify-center items-center text-center p-5 m-5">Battle Buddies Homepage!</h1>
+
+        <div className="flex flex-row items-center justify-center h-full w-full">
+          <BattleHistory />
+        </div>
+
       </main>
+      
+      
     </div>
   );
 }
