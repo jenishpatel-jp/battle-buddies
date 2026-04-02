f3fa3e0cc079b9243d210ab852027faeada0671c
Jenish <jpat534@gmail.com>
2026-04-02 17:10:35 +1100

    Imported Card from ShadCN

--- components/battleHistory/BattleHistory.tsx
+++ components/battleHistory/BattleHistory.tsx
@@ -1,15 +1,15 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
 const BattleHistory = () => {
   return (
     <div className="flex flex-col border-2 border-blue-500 rounded-lg p-4 m-4 w-full max-w-md">
-        <h2 className="text-black text-2xl text-center">Battle History</h2>
-        <div className="flex flex-row items-center w-full justify-around mt-4">
-            <h2 className="text-black items-center justify-center p-2 text-center text-4xl">213</h2>
-            <h2 className="text-black items-center justify-center p-2 text-center text-4xl">300</h2>
-        </div>
-        <div className="flex flex-row items-center w-full justify-around mt-4">
-            <h2 className="text-black items-center justify-center p-2 text-center text-xl">Jenish</h2>
-            <h2 className="text-black items-center justify-center p-2 text-center text-xl">Daniel</h2>
-        </div>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-center text-2xl">Battle History</CardTitle>
+
+        </CardHeader>
+      </Card>
+
     </div>
   )
 };
