680ce083ec1f504662d7c7f290798ca8b17e6e1a
Jenish <jpat534@gmail.com>
2026-04-03 12:25:11 +1100

    Added carousel for battle history components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,29 @@
+'use client'
+
 import BattleHistory from "../components/battleHistory/BattleHistory";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-1/6 items-center justify-center bg-zinc-50 font-sans ">
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans ">
       
-      <main className="flex min-h-screen flex-col items-center justify-center py-32 px-16 bg-zinc sm:items-start w-full">
-        <h1 className="text-4xl font-extrabold w-full text-black justify-center items-center text-center p-5 m-5">Scoreboard!</h1>
+      <main className="flex flex-col items-center justify-center w-full py-16 px-6">
+        <h1 className="text-4xl font-extrabold w-full text-black text-center p-5 m-5">Scoreboard!</h1>
 
-        <div className="flex flex-row items-center justify-center h-full w-full">
-          <BattleHistory />
+        <div className="flex items-center justify-center w-full max-w-4xl">
+          <Carousel className="w-full ">
+            <CarouselContent >
+              <CarouselItem className="flex items-center justify-center w-full">
+                <BattleHistory />
+              </CarouselItem>
+              <CarouselItem className="flex items-center justify-center w-full">
+                <BattleHistory />
+              </CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+            
+          </Carousel>
         </div>
 
       </main>
