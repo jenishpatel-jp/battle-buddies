28db50c89c650eeab2b851272d46523a085f3102
Jenish <jpat534@gmail.com>
2026-04-06 16:25:37 +1000

    Created a Manager Players component and used the card component from Shadcn

---
+++ src/app/players/page.tsx
@@ -0,0 +1,9 @@
+import ManagePlayers from "@/components/players/ManagePlayers";
+
+export default function AddPlayer() {
+    return(
+        <div className="flex flex-1 min-h-screen bg-zinc-50 items-center justify-center flex-col">
+           <ManagePlayers />
+        </div>
+    )
+};
---
+++ src/components/players/ManagePlayers.tsx
@@ -0,0 +1,11 @@
+import { Card, CardTitle } from "../ui/card"
+
+const ManagePlayers = () => {
+    return (
+        <Card>
+            <CardTitle className="text-center text-4xl text-blue-500 font-semibold" >Manage Players</CardTitle>
+        </Card>
+    )
+}
+
+export default ManagePlayers
