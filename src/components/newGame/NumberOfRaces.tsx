dc64952c97dcdf39cdf27e1f26b7a497c5d07c52
Jenish <jpat534@gmail.com>
2026-04-08 18:27:45 +1000

    Added buttons for the number of races

--- src/components/newGame/NumberOfRaces.tsx
+++ src/components/newGame/NumberOfRaces.tsx
@@ -1,4 +1,5 @@
-import { Card, CardHeader, CardTitle } from "../ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card"
+import { Button } from "../ui/button"
 
 const NumberOfRaces = () => {
     return (
@@ -6,6 +7,14 @@ const NumberOfRaces = () => {
             <CardHeader >
                 <CardTitle className="text-center text-4xl text-blue-500 font-semibold">Select the number of races</CardTitle>
             </CardHeader>
+            <CardContent className="h-full flex flex-row items-center justify-center flex-wrap gap-4">
+                <Button className="p-3 m-3 w-1/3 h-1/5 bg-blue-300 text-xl">4</Button>
+                <Button className="p-3 m-3 w-1/3 h-1/5 bg-blue-300 text-xl">8</Button>
+                <Button className="p-3 m-3 w-1/3 h-1/5 bg-blue-300 text-xl">12</Button>
+                <Button className="p-3 m-3 w-1/3 h-1/5 bg-blue-300 text-xl">16</Button>
+                <Button className="p-3 m-3 w-1/3 h-1/5 bg-blue-300 text-xl">24</Button>
+                <Button className="p-3 m-3 w-1/3 h-1/5 bg-blue-300 text-xl">32</Button>
+            </CardContent>
 
         </Card>
     )
