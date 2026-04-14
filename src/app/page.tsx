23cc65fe4b18db15f2ba3bef9b76f428bb1079fa
Jenish <jpat534@gmail.com>
2026-04-14 18:37:00 +1000

    removed the imports that were being used in main app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 'use client'
 
-import BattleHistory from "../components/battleHistory/BattleHistory";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
-import { Button } from "@/components/ui/button";
 import Modal from "@/components/modal/Modal";
 import { useState } from "react";
 
 export default function Home() {
 
   const [isOpen, setIsOpen] = useState(false);
- 
-  
-
-
 
   return (
     <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans ">
