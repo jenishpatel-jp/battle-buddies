811da5122ea56dd3ea76f30157e3156977643844
Jenish <jpat534@gmail.com>
2026-04-14 17:13:48 +1000

    Changed the text back to buttons so that way the useState can be changed when clicked

--- src/components/newGame/NumberOfRaces.tsx
+++ src/components/newGame/NumberOfRaces.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card"
 import { Button } from "../ui/button"
+import Link from "next/link"
 
 const NumberOfRaces = () => {
     return (
@@ -17,8 +18,12 @@ const NumberOfRaces = () => {
                 <Button className="p-3 m-3 w-1/3 h-1/5 bg-blue-300 text-xl">32</Button>
             </CardContent>
             <div className=" flex flex-row items-center justify-around">
+                <Button className="p-3 m-3 w-1/3 h-1/5 bg-white text-black text-xl flex items-center justify-center rounded-lg">
                     <h2 className="text-2xl p-2">Back</h2>
+                </Button>
+                <Button  className="p-3 m-3 w-1/3 h-1/5 bg-white text-black text-xl flex items-center justify-center rounded-lg">
                     <h2 className="text-2xl p-2">Next</h2>
+                </Button>
             </div>
 
         </Card>
