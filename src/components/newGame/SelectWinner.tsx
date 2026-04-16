f89f291d465f693f289934c89e6a8068f11b9467
Jenish <jpat534@gmail.com>
2026-04-16 17:45:03 +1000

    Update the Select winner card

--- src/components/newGame/SelectWinner.tsx
+++ src/components/newGame/SelectWinner.tsx
@@ -7,9 +7,10 @@ const SelectWinner = () => {
     return (
         <Card className="flex flex-col p-4 m-4 w-full max-w-md shadow-lg">
             <CardHeader >
-                <CardTitle className="text-center text-4xl text-blue-500 font-semibold">Manage Players</CardTitle>
+                <CardTitle className="text-center text-4xl text-blue-500 font-semibold">Who Won?</CardTitle>
             </CardHeader>
             <CardContent className="h-full">
+                <h2>Match 1</h2>
                 <div className="grid grid-cols-2 gap-4 p-2">
 
                     <h2 className="text-xl text-center">1</h2>
@@ -20,8 +21,13 @@ const SelectWinner = () => {
                     <h2 className="text-xl text-center">Ashwin</h2>
 
                 </div>
-                <div className=" flex flex-row items-center justify-center ">
-                    <Button className="p-3 m-3 w-1/3 h-1/5 bg-blue-300 text-xl">Add Player</Button>
+                <div className="flex flex-row items-center justify-around p-2">
+                    <div className=" flex flex-row items-center justify-center ">
+                        <Button className="p-3 m-3 w-1/3 h-1/3 bg-white text-xl text-black">Back</Button>
+                    </div>
+                    <div className=" flex flex-row items-center justify-center ">
+                        <Button className="p-3 m-3 w-1/3 h-1/3 bg-white text-xl text-black">Next</Button>
+                    </div>
                 </div>
             </CardContent>
         </Card>
