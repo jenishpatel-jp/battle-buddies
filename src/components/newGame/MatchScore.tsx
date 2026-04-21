7b9689d9f344e295ad9d8a1cfb42b1f5e1e65c68
Jenish <jpat534@gmail.com>
2026-04-21 17:31:49 +1000

    Added a MatchScore component

---
+++ src/components/newGame/MatchScore.tsx
@@ -0,0 +1,21 @@
+import { Card, CardTitle, CardDescription, CardHeader, CardContent } from "../ui/card"
+
+const MatchScore = () => {
+
+    return (
+
+        <Card className="flex flex-col p-4 m-4 w-full max-w-md shadow-lg">
+            <CardHeader>
+                <CardTitle className="text-center text-2xl">Race 1 finished Match Score</CardTitle>
+                <h4 className="text-center text-lg text-muted-foreground">Date: {new Date().toLocaleDateString()}</h4>
+                {/* Date */}
+            </CardHeader>
+            <CardContent>
+                {/* Match history details will go here */}
+            </CardContent>
+        </Card>
+
+    )
+}
+
+export default MatchScore
